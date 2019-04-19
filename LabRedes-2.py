6987448a342f115c3dfe81cae3e610488c35e81e
asdfghjkln1 <38445887+asdfghjkln1@users.noreply.github.com>
2019-04-19 11:50:12 -0400

    se ordena codigo

--- LabRedes-2.py
+++ LabRedes-2.py
@@ -1,18 +1,18 @@
 import matplotlib.pyplot as plt
 from scipy.io.wavfile import read, write
-from scipy.fftpack import fft, fftfreq, ifft
+from scipy.fftpack import fft, fftfreq, ifft, fftn
 import numpy as np
 import wavio
 
 
 datos = read("handel.wav")
-frate = datos[0]
+srate = datos[0]
 data = datos[1]
 
-print(f'El archivo tiene una frecuencia de muestreo de {frate}')
+print(f'El archivo tiene una frecuencia de muestreo de {srate}')
 # Cálculo del eje X, correspondiente al tiempo del grafico del data
 n = len(data)  # Numero de intervalos
-dt = 1 / frate  # Delta tiempo entre cada intervalo
+dt = 1 / srate  # Delta tiempo entre cada intervalo
 largo = n * dt  # Tiempo total para generar n puntos de espaciado dt
 vector_tiempo = np.linspace(0, largo, num=n)  # Generación del vector de tiempos
 
@@ -21,24 +21,25 @@ print(f'El tiempo entre cada punto es {dt}, el tiempo total es {largo}')
 fft_out = fft(data)
 frq = fftfreq(n, dt)  # Vector de frecuencias para la transformada (Eje X)
 
-
+# fft_norm = list(map(lambda x: x/n, fft_out))  # No se requiere una normalización
 fft_norm = fft_out
-#fft_norm = list(map(lambda x: x/n, fft_out))  # Se deberia escalar por n o por n/2?
 
-
-deltaFrq = 0.1 # Al cambiar esto se seleccionan las frecuencias alrededor de la frecuencia de magnitud máxima
+deltaFrq = 1 # Al cambiar esto se seleccionan las frecuencias al rededor de la frecuencia de magnitud máxima
+# deltaFrq = 0.1 # Descomentar para prueba 2
 
 maxFrqIndex = np.argmax(fft_norm) # Índice frecuencia máxima
 filterfrq = frq[maxFrqIndex] # Frecuencia máxima
 limFrqInf = filterfrq - deltaFrq
 limFrqSup = filterfrq + deltaFrq
 
+diffFrec = srate/n # Espacio diferencial del vector de frecuencias
+puntos = 2*deltaFrq//diffFrec # Número de puntos tomados en el intervalo de filtrado
+print(f'Se toma el rango de {deltaFrq} [Hz] alrededor de la frecuencia predominante')
 print(f'La frecuencia central de filtrado es {filterfrq} de indice {maxFrqIndex}, se seleccionan las frecuencias entre {limFrqInf} y {limFrqSup} Hz')
+print(f'Se seleccionan {puntos} frecuencia/s en este rango')
 
 # Filtración del % de frecuencias al rededor del máximo
-
 fft_fixed = list(map(lambda x: 0 if (frq[x[0]] < limFrqInf or frq[x[0]] > limFrqSup ) else x[1], enumerate(np.abs(fft_norm))))
-
 
 # Audio original
 plt.figure(1)
@@ -52,7 +53,7 @@ plt.ylabel('Amplitud')
 plt.figure(2)
 plt.plot(frq, np.abs(fft_norm), 'r')
 
-plt.title('Transformada de Fourier del audio')
+plt.title('Transformada de Fourier')
 plt.xlabel('Frecuencia [Hz]')
 plt.ylabel('|F(w)|')
 #plt.show()
@@ -69,10 +70,10 @@ plt.ylabel('Amplitud')
 # Transformada truncada
 plt.figure(4)
 plt.plot(frq, np.abs(fft_fixed), 'g')
-plt.title('Transformada de Fourier Truncada')
+plt.title('Transformada de Fourier truncada')
 plt.xlabel('Frecuencia [Hz]')
 plt.ylabel('|F(w)|')
-plt.show()
+#plt.show()
 
 # Audio transformada inversa truncada
 plt.figure(5)
@@ -83,7 +84,15 @@ plt.xlabel('Tiempo [s]')
 plt.ylabel('Amplitud')
 plt.show()
 
-wavio.write("handel_inversa_1.wav", fftinv, frate, sampwidth=3) # Metodo 2 escritura en .wav
-write('handel_inversa_2.wav', frate, fftinv)
+# Audio transformada inversa sin filtrar
+wavio.write("handel_inversa_original.wav", fft_inversa, srate, sampwidth=3) # Metodo 2 escritura en .wav
+
+# Audio luego del filtrado
+wavio.write("handel_inversa_fixed.wav", fftinv, srate, sampwidth=3) # Metodo 2 escritura en .wav
+
+# En caso de que falle la escritura de archivo descomentar
+# write('handel_fftinv_original.wav', rate, fftinv) # Método 1 escritura en .wav
+# write('handel_fftinv_inversa_fixed.wav', rate, fftinv) # Método 1 escritura en .wav
+
 
 print('Programa finalizado con éxito')
